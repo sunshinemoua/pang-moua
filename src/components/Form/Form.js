e24b79fb87bc23aa7870298afe1cc65f0079375f
Sunshine Moua <sunshinemoua@Sunshines-MacBook-Pro.local>
2023-03-10 16:30:20 -0800

    added verification to form; requires all fields; provides error message if field is missing

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import Card from "../Card/Card";
-import Button from "../Button/Button";
 import Header from "../Header/Header";
 import classes from "./Form.module.css";
 import emailjs from "@emailjs/browser";
@@ -10,9 +9,9 @@ const Form = () => {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
 
-  const sendEmail = (event) => {
-    event.preventDefault();
+  const [clicked, setClicked] = useState(false);
 
+  const sendEmail = () => {
     const templateParams = {
       to_name: "Sunshine",
       from_name,
@@ -30,7 +29,7 @@ const Form = () => {
       .then(
         (result) => {
           console.log(result.text);
-          alert("message sent successfully");
+          alert("Thank you for your message!");
           setFromName("");
           setEmail("");
           setMessage("");
@@ -41,43 +40,70 @@ const Form = () => {
       );
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (from_name && email && message) {
+      sendEmail();
+      setClicked(false);
+    } else {
+      setClicked(true);
+    }
+  };
+
   return (
     <div>
       <Card className={classes.background}>
         <div className={classes["form-outer-wrapper"]}>
           <Header header={"Get in Touch With Me"} />
-          <form className={classes["form-inner-wrapper"]} onSubmit={sendEmail}>
+          <form
+            className={classes["form-inner-wrapper"]}
+            onSubmit={handleSubmit}
+          >
             <span className={classes["full-name-email-row"]}>
-              <input
-                className={classes["input-name"]}
-                placeholder="Full Name"
-                type="text"
-                name="name"
-                value={from_name}
-                onChange={(event) => setFromName(event.target.value)}
-              />
-              <input
-                className={classes["input-email"]}
-                placeholder="Email"
-                type="email"
-                id="email"
-                value={email}
-                onChange={(event) => setEmail(event.target.value)}
-              />
+              <span className={classes["full-name-error"]}>
+                <input
+                  className={classes["input-name"]}
+                  placeholder="Full Name"
+                  type="text"
+                  name="name"
+                  value={from_name}
+                  onChange={(event) => setFromName(event.target.value)}
+                />
+                {!from_name && clicked && (
+                  <span className={classes.error}>Please enter a name</span>
+                )}
+              </span>
+              <span className={classes["email-error"]}>
+                <input
+                  className={classes["input-email"]}
+                  placeholder="Email"
+                  type="email"
+                  id="email"
+                  value={email}
+                  onChange={(event) => setEmail(event.target.value)}
+                />
+
+                {!email && clicked && (
+                  <span className={classes.error}>Please enter an email</span>
+                )}
+              </span>
             </span>
-            <textarea
-              className={classes["input-message"]}
-              placeholder="Message..."
-              type="text"
-              name="message"
-              value={message}
-              onChange={(event) => setMessage(event.target.value)}
-            />
+            <span className={classes["message-error"]}>
+              <textarea
+                className={classes["input-message"]}
+                placeholder="Message..."
+                type="text"
+                name="message"
+                value={message}
+                onChange={(event) => setMessage(event.target.value)}
+              />
+              {!message && clicked && (
+                <span className={classes.error}>Please enter a message</span>
+              )}
+            </span>
 
             <span className={classes.button}>
-              <Button className={classes.sendButton} type="submit">
-                Send
-              </Button>
+              <button type="submit">Send</button>
             </span>
           </form>
         </div>
