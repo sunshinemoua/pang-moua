33c6183d0e04d5092209f837370907534725938a
Sunshine Moua <sunshinemoua@Sunshines-MacBook-Pro.local>
2023-03-28 20:49:46 -0700

    fixed margin in contact page and background height in portfolio page

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -52,57 +52,59 @@ const Form = () => {
 
   return (
     <Card className={classes.background}>
-      <Header header={"Get in Touch With Me"} />
-      <form className={classes.wrapper} onSubmit={handleSubmit}>
-        <div className={classes["inner-wrapper"]}>
-          <div className={classes["full-name-email"]}>
-            <div className={classes["input-one"]}>
-              <input
-                className={classes["input-two"]}
-                placeholder="Full Name"
-                type="text"
-                name="name"
-                value={from_name}
-                onChange={(event) => setFromName(event.target.value)}
-              />
-              {!from_name && clicked && (
-                <span className={classes.error}>Please enter a name</span>
-              )}
-            </div>
-            <div className={classes["input-one"]}>
-              <input
-                className={classes["input-two"]}
-                placeholder="Email"
-                type="email"
-                id="email"
-                value={email}
-                onChange={(event) => setEmail(event.target.value)}
-              />
+      <div className={classes["outer-div"]}>
+        <Header header={"Get in Touch With Me"} />
+        <form className={classes.wrapper} onSubmit={handleSubmit}>
+          <div className={classes["inner-wrapper"]}>
+            <div className={classes["full-name-email"]}>
+              <div className={classes["input-one"]}>
+                <input
+                  className={classes["input-two"]}
+                  placeholder="Full Name"
+                  type="text"
+                  name="name"
+                  value={from_name}
+                  onChange={(event) => setFromName(event.target.value)}
+                />
+                {!from_name && clicked && (
+                  <span className={classes.error}>Please enter a name</span>
+                )}
+              </div>
+              <div className={classes["input-one"]}>
+                <input
+                  className={classes["input-two"]}
+                  placeholder="Email"
+                  type="email"
+                  id="email"
+                  value={email}
+                  onChange={(event) => setEmail(event.target.value)}
+                />
 
-              {!email && clicked && (
-                <span className={classes.error}>Please enter an email</span>
+                {!email && clicked && (
+                  <span className={classes.error}>Please enter an email</span>
+                )}
+              </div>
+            </div>
+            <div className={classes.message}>
+              <textarea
+                className={classes["input-two"]}
+                placeholder="Message..."
+                type="text"
+                name="message"
+                value={message}
+                rows="15"
+                onChange={(event) => setMessage(event.target.value)}
+              />
+              {!message && clicked && (
+                <span className={classes.error}>Please enter a message</span>
               )}
+              <div className={classes.button}>
+                <button type="submit">Send</button>
+              </div>
             </div>
           </div>
-          <div className={classes.message}>
-            <textarea
-              className={classes["input-two"]}
-              placeholder="Message..."
-              type="text"
-              name="message"
-              value={message}
-              rows="15"
-              onChange={(event) => setMessage(event.target.value)}
-            />
-            {!message && clicked && (
-              <span className={classes.error}>Please enter a message</span>
-            )}
-            <div className={classes.button}>
-              <button type="submit">Send</button>
-            </div>
-          </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </Card>
   );
 };
